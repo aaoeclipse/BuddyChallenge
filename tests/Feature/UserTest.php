43c735281dc20b1ae9133db61a9183f68ee3e452
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-13 12:01:12 +0100

    test: :white_check_mark: add default test to check auth

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_it_stores_new_user()
+    {
+        $response = $this->post('/register', [
+            'name' => 'Rick Sanchez',
+            'email' => 'ricksanchez@gmail.com',
+            'password' => 'pass123!This',
+            'password_confirmation' => 'pass123!This',
+        ]);
+
+        $response->assertRedirect('/home');
+    }
+
+    public function test_delete_user()
+    {
+        $user = User::where('name', 'Rick Sanchez')->first();
+
+        if ($user) {
+            $user->delete();
+            $this->assertTrue(true);
+        }
+
+        $this->assertDatabaseMissing('users', ['name' => 'Rick Sanchez']);
+    }
+}
