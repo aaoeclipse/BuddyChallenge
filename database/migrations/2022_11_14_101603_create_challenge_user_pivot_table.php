53420d83990c0c3d2e2f9cde2a9a9cf9385367d7
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-14 11:31:05 +0100

    feat: :sparkles: add first pivot table

---
+++ database/migrations/2022_11_14_101603_create_challenge_user_pivot_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenge_user', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('challenge_id')->unsigned()->index();
+            $table->foreign('challenge_id')->references('id')->on('challenges')->onDelete('cascade');
+            $table->primary(['user_id', 'challenge_id']);
+
+            $table->integer('points')->default(0);
+            $table->boolean('accepted')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenge_user');
+    }
+};
