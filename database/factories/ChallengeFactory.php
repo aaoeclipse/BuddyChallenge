f228e4385bc23f9ca822bf09cf35663aa7aa6d9f
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-14 14:01:26 +0100

    feat: :hammer: add factory for challenge

---
+++ database/factories/ChallengeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Challenge>
+ */
+class ChallengeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'starting_date' => fake()->date(),
+            'ending_date' => fake()->date(),
+            'title' => fake()->name(),
+            'description' => fake()->text(),
+            'owner_id' => User::class::factory(),
+        ];
+    }
+}
--- database/migrations/2022_11_14_101603_create_challenge_user_pivot_table.php
+++ database/migrations/2022_11_14_101603_create_challenge_user_pivot_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
 
             $table->integer('points')->default(0);
             $table->boolean('accepted')->default(false);
+            $table->timestamps();
         });
     }
 
