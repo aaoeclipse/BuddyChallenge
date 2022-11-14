9afd5931b4988f3fa2e6fbcc87df66320e501353
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-14 15:01:31 +0100

    style: :bulb: update comments on the route web.php

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,14 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// PUBLIC ROUTES
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
+
+// PRIVATE ROUTES
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
