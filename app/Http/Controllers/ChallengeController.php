fc201fde529226bf206ac5ad448b876a24fb72c8
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-15 11:39:46 +0100

    feat: :sparkles: add index show all challenges to user

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChallengeController extends Controller
 {
@@ -13,7 +14,9 @@ class ChallengeController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+
+        return view('challenges', ['challenges' => $user->own_challenges]);
     }
 
     /**
