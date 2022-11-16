94d50d1dfd35a0231e4bf134959e91b77328f1b6
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-16 10:11:24 +0100

    feat: :test_tube: add test for update challenge

--- tests/Feature/ChallengeTest.php
+++ tests/Feature/ChallengeTest.php
@@ -11,10 +11,13 @@ class ChallengeTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $user;
+
     public function setUp(): void
     {
         parent::setUp();
         $this->seed();
+        $this->user = User::where('email', 'santiago.paiz@gmail.com')->first();
     }
 
     /**
@@ -32,35 +35,30 @@ class ChallengeTest extends TestCase
 
     public function test_get_all_challenges()
     {
-        $user = User::where('email', 'santiago.paiz@gmail.com')->first();
-
-        $response = $this->actingAs($user)->get('/challenge');
+        $response = $this->actingAs($this->user)->get('/challenge');
 
         $response->assertSee('Starting');
     }
 
     public function test_get_specific_challenges()
     {
-        $user = User::where('email', 'santiago.paiz@gmail.com')->first();
-        $challenge = $user->own_challenges->first();
+        $challenge = $this->user->own_challenges->first();
 
-        $response = $this->actingAs($user)->get('/challenge/'.$challenge['id']);
+        $response = $this->actingAs($this->user)->get('/challenge/'.$challenge['id']);
 
         $response->assertStatus(200);
     }
 
     public function test_user_create_challenge()
     {
-        $user = User::where('email', 'santiago.paiz@gmail.com')->first();
-
         $challenge = new Challenge();
         $challenge->starting_date = now();
         $challenge->ending_date = now()->addDay();
         $challenge->title = 'New Title';
         $challenge->description = 'New Description';
-        $challenge->owner_id = $user->id;
+        $challenge->owner_id = $this->user->id;
 
-        $response = $this->actingAs($user)->post('/challenge', [
+        $response = $this->actingAs($this->user)->post('/challenge', [
             'title' => $challenge->title,
             'description' => $challenge->description,
             'starting_date' => $challenge->starting_date,
@@ -73,16 +71,27 @@ class ChallengeTest extends TestCase
         $this->assertDatabaseHas('challenges', ['title' => 'New Title']);
         // Make sure the challenge_user was correctly created
         $challenge = Challenge::where('title', 'New Title')->first();
-        $this->assertDatabaseHas('challenge_user', ['user_id' => $user->id, 'challenge_id' => $challenge->id]);
+        $this->assertDatabaseHas('challenge_user', ['user_id' => $this->user->id, 'challenge_id' => $challenge->id]);
+    }
+
+    public function test_update_challenge()
+    {
+        $challenge = $this->user->own_challenges->first();
+        $response = $this->actingAs($this->user)->put('/challenge/'.$challenge->id, [
+            'title' => 'New New title',
+            'description' => $challenge->description,
+            'starting_date' => $challenge->starting_date,
+            'ending_date' => $challenge->ending_date,
+        ]);
+        $this->assertDatabaseHas('challenges', ['title' => 'New New title']);
     }
 
     public function test_delete_challenge()
     {
-        $user = User::where('email', 'santiago.paiz@gmail.com')->first();
-        $challenge = $user->own_challenges->first();
+        $challenge = $this->user->own_challenges->first();
         $this->assertDatabaseHas('challenges', ['id' => $challenge->id]);
 
-        $this->actingAs($user)->delete('/challenge/'.$challenge['id']);
+        $this->actingAs($this->user)->delete('/challenge/'.$challenge['id']);
         $this->assertDatabaseMissing('challenges', ['id' => $challenge->id]);
     }
 }
