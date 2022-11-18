875cef6b51a253d888c008b5e1a98af077651ade
Santiago Paiz <santiago.paiz@gmail.com>
2022-11-18 09:45:14 +0100

    fix: :art: decompose dashboard(home)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+        <div class="row justify-content-center">
+            <div class="col-md-16 flex flex-col gap-4">
+                <div class="card">
+                    <div class="card-body">
+                        <x-own-components title="Current Challenges" :challenges='$user->challenges' admin='false'></x-own-components>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body flex flex-col">
+                        <x-own-components title="Own Challenges" :challenges='$user->own_challenges' admin='true'></x-own-components>
+                        <a href="{{ route('challenge.create') }}" class="btn btn-primary mt-3">+
+                            Challenge</a>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
-</div>
 @endsection
